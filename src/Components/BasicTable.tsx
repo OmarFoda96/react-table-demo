a5936a04cc8a24537b8f1581b28e952aebaa4a8c
OmarFoda96 <OmarFoda96@gmail.com>
2021-08-12 16:26:04 +0200

    push table array headers

--- src/Components/BasicTable.tsx
+++ src/Components/BasicTable.tsx
@@ -1,43 +1,52 @@
 import React, { useMemo } from "react";
 
-import {useTable} from "react-table";
+import { useTable } from "react-table";
 import Columns from "./columns";
 import MOCK_DATA from "./MOCK_DATA.json";
-import './basicTable.css'
+import "./basicTable.css";
 
 const BasicTable: React.FC = () => {
   const columns = useMemo(() => Columns, []);
   const data = useMemo(() => MOCK_DATA, []);
 
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable<any>({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow,footerGroups } =
+    useTable<any>({ columns, data });
   return (
     <table {...getTableProps()}>
       <thead>
         {headerGroups.map((headerGroup: any) => {
-          <tr {...headerGroup.getHeaderGroupProps()}>
+         return <tr  {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column: any) => {
-              <th {...column.getHeaderProps}>{column.render("Header")}</th>;
+              return (
+                <th key={column.id} {...column.getHeaderProps}>{column.render("Header")}</th>
+              );
             })}
           </tr>;
         })}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {
-          rows.map((row:any)=>{
-            prepareRow(row)
-            return <tr {...row.getRowProps()}>
-              {row.cells.map((cell:any)=>{
-                return <td {...cell.getCellProps()}>
-                  {cell.render('Cell')}
-                </td>
+        {rows.map((row: any) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell: any) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
               })}
             </tr>
-          })
-        }
-        <tr>
-          <td></td>
-        </tr>
+          );
+        })}
       </tbody>
+      <tfoot>
+        {footerGroups.map((footerGroup: any) => {
+         return <tr  {...footerGroup.getHeaderGroupProps()}>
+            {footerGroup.headers.map((column: any) => {
+              return (
+                <th key={column.id} {...column.getFooterProps}>{column.render("Footer")}</th>
+              );
+            })}
+          </tr>;
+        })}
+      </tfoot>
     </table>
   );
 };
--- src/Components/columns.ts
+++ src/Components/columns.ts
@@ -1,39 +1,48 @@
 export interface ColumnModel {
   accessor: string;
   Header:string;
+  Footer:string;
 }
 
 const Columns = [
   {
     Header: "id",
+    Footer: "id",
     accessor: "id",
   },
   {
     Header: "First Name",
+    Footer: "First Name",
     accessor: "first_name",
   },
   {
     Header: "Last Name",
+    Footer: "Last Name",
     accessor: "last_name",
   },
   {
     Header: "Email",
+    Footer: "Email",
     accessor: "email",
   },
   {
     Header: "Birthday",
+    Footer: "Birthday",
     accessor: "date_of_birth",
   },
   {
     Header: "Age",
+    Footer: "Age",
     accessor: "age",
   },
   {
     Header: "Country",
+    Footer: "Country",
     accessor: "country",
   },
   {
     Header: "Phone",
+    Footer: "Phone",
     accessor: "phone",
   },
 ];
